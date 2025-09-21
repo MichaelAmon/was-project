c5490b68fccf6ef211c7ce27af42f312fefceba1
MichaelAmon <70228522+MichaelAmon@users.noreply.github.com>
2025-09-21 01:14:09 -1000

    Update index.js

--- index.js
+++ index.js
@@ -179,6 +179,7 @@ process.on("unhandledRejection", err => {
 
      // Start app on VPS
      app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
+        app.listen(process.env.PORT || 3000)
       //  app.listen(port, '0.0.0.0', () => {
        //   console.log(`Server running on port ${port}`);
 });
