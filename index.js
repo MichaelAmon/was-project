f4734e7f52a85fcf202368787516045261b74d30
MichaelAmon <70228522+MichaelAmon@users.noreply.github.com>
2025-09-21 07:57:20 -1000

    Update index.js

--- index.js
+++ index.js
@@ -177,7 +177,7 @@
      }
 
      // Start app on VPS
-     //app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
-     app.listen(PORT, '0.0.0.0', () => {
-  console.log(`Server running on port ${PORT}`);
+     app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
+    
+
 
