0a8bdb9b911f652a57d3ca90dca1d73a9b2ecdde
MichaelAmon <70228522+MichaelAmon@users.noreply.github.com>
2025-09-21 14:34:26 -1000

    Update index.js

--- index.js
+++ index.js
@@ -59,7 +59,7 @@
      const serviceAccountAuth = new JWT({
        email: process.env.GOOGLE_SERVICE_ACCOUNT_EMAIL,
        //key: process.env.GOOGLE_PRIVATE_KEY.replace(/\\n/g, '\n'),
-          key: process.env.GOOGLE_PRIVATE_KEY.replace(/\\\\n/g, '\n'),
+          //key: process.env.GOOGLE_PRIVATE_KEY.replace(/\\\\n/g, '\n'),
        scopes: ['https://www.googleapis.com/auth/spreadsheets']
      });
 
@@ -195,6 +195,7 @@
 app.listen(PORT, '0.0.0.0', () => {
   console.log(`🎉 Attendance app running on http://0.0.0.0:${PORT}`);
 });
+
 
 
 
